69086d33a972df34b1dad7ba891577073ff71f6c
Charleira <cadupiacente67@gmail.com>
2024-08-11 01:20:11 -0300

    mudanças site

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 // Certifique-se de definir corretamente os componentes que está importando
-import Header from './components/Header';
+import Header from './components/Headertop';
 import HeroSection from './components/HeroSection';
 import CoursesSection from './components/CoursesSection';
 import HowItWorksSection from './components/HowItWorksSection';
